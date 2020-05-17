e2255d860ec5ee0797bdbf0c79281a2bb4adedf1
JUAN CARLOS CATTANEO <juancattaneo92@gmail.com>
2020-05-17 01:49:39 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -5,5 +5,21 @@ def position_taken?(board, index)
     return false 
   elsif board[1] == " "
   return false
-  
+  elsif board[2] == " "
+  return false
+  elsif board[3] == " "
+  return false
+  elsif board[4] == " "
+  return false
+  elsif board[5] == " "
+  return false
+  elsif board[6] == " "
+  return false
+  elsif board[7] == " "
+  return false
+  elsif board[8] == " "
+  return false
+  else true
+  end
 end
+
