ca6491641205e8a3cbb41c1b604e20cfaa19c237
JUAN CARLOS CATTANEO <juancattaneo92@gmail.com>
2020-05-17 01:54:32 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,25 +1,8 @@
 # code your #position_taken? method here!
 
 def position_taken?(board, index)
-  if board[0] == " "
+  if board[index] == " " || board[index] == ""
     return false 
-  elsif board[1] == " "
-  return false
-  elsif board[2] == " "
-  return false
-  elsif board[3] == " "
-  return false
-  elsif board[4] == " "
-  return false
-  elsif board[5] == " "
-  return false
-  elsif board[6] == " "
-  return false
-  elsif board[7] == " "
-  return false
-  elsif board[8] == " "
-  return false
-  else true
-  end
+    
 end
 
